afd1542639be8b2b09b7e4283b684cc764dce61d
Wes Griffin <typicalgames@gmail.com>
2019-08-30 17:31:54 +0000

    planet form submits to index

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -6,4 +6,15 @@ class PlanetsController < ApplicationController
   def new
     @planet = Planet.new
   end
+
+  def create
+    Planet.create(planet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :description)
+  end
 end
